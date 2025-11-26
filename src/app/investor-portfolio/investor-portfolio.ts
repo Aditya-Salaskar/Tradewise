237a18a4fa617a2424638647de3552cf4167e1fe
Phapale <2457188@cognizant.com>
2025-11-26 10:45:48 +0530

    Created dashboard and portfolio for investor component

---
+++ src/app/investor-portfolio/investor-portfolio.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investor-portfolio',
+  imports: [],
+  templateUrl: './investor-portfolio.html',
+  styleUrl: './investor-portfolio.css',
+})
+export class InvestorPortfolio {
+
+}
