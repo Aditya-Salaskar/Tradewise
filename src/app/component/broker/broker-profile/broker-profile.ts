9ab71c904a4b3f0548244487ec45bb4d37e68b4c
Singh <2457299@cognizant.com>
2025-11-29 14:38:15 +0530

    Created broker risk analysis

---
+++ src/app/component/broker/broker-profile/broker-profile.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-broker-profile',
+  standalone: true,
+  templateUrl: './broker-profile.html',
+  styleUrls: ['./broker-profile.css']
+})
+export class BrokerProfile {}
