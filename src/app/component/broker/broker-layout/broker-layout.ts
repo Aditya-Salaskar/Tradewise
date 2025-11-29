0c94517b3baa2ddb4931887ecd3c5a9a6ec5b6e2
Singh <2457299@cognizant.com>
2025-11-29 23:11:17 +0530

    broker nav bar completed added everything profile and all dark and night mode feature added

--- src/app/component/broker/broker-layout/broker-layout.ts
+++ src/app/component/broker/broker-layout/broker-layout.ts
@@ -8,4 +8,16 @@ import { RouterModule } from '@angular/router';
   templateUrl: './broker-layout.html',
   styleUrls: ['./broker-layout.css']
 })
-export class BrokerLayout {}
+export class BrokerLayout {
+
+  toggleTheme(event: any) {
+    const enabled = event.target.checked;
+
+    if (enabled) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }
+
+}
